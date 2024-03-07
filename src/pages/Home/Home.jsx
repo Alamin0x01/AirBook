3c6057fa98f37a333b47e3d1647bd6b862fe88b6
Md. Alamin A <md.alamin.ali.096@gmail.com>
2024-03-07 18:48:48 +0600

    Banner-new , footer -- added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,20 +5,21 @@ import Featured from "../../components/Featured/Featured";
 import Navbar from "../../components/Navbar/Navbar";
 import Newsletter from "../../components/Newsletter/Newsletter";
 import Testimonial from "../../components/Testimonial/Testimonial";
-
+import Footer from "../../components/footer";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Banner></Banner>
-            <Featured></Featured>
-            <Deals></Deals>
-            <Airlines></Airlines>
-            <Testimonial></Testimonial>
-            <Newsletter></Newsletter>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Featured></Featured>
+      <Deals></Deals>
+      <Airlines></Airlines>
+      <Testimonial></Testimonial>
+      <Newsletter></Newsletter>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
