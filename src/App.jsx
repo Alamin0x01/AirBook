7a811060639638404378e316bcfe188da4aca74f
Md. Alamin A <md.alamin.ali.096@gmail.com>
2024-03-07 11:14:51 +0600

    react app setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>React App</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex justify-start">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
